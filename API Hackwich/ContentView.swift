e1c8bb65be2fb9c8711b70c602583df2a12a487f
Dylan Koehler <Dylankoehler3212@gmail.com>
2022-02-02 10:35:12 -0600

    Got Api Key

--- API Hackwich/ContentView.swift
+++ API Hackwich/ContentView.swift
@@ -26,9 +26,7 @@ struct ContentView: View {
         })
     }
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?", punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?", punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world", punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=4ce364c2b6mshcf80236d4a1cde8p11d71ejsnd86a4fd32ac0"
     }
     
 }
